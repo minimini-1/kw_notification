e2d61889a979d0e09c1a84dfa8a28857d53e128a
kwjinwoo <38836328+kwjinwoo@users.noreply.github.com>
2020-06-30 15:21:51 +0900

    하나로 합친 파일
    제목과 날짜를 통해 오늘 날짜 기준 새로 등록된 것, 수정된 것의 제목을 출력
    사용자가 확인 안하고 새롭게 등록된 공지를 알려주려면 이 방법말고 다른 방법이 필요할듯

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+url = 'https://www.kw.ac.kr/ko/life/notice.jsp'
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+req = requests.get(url, headers=header)
+html = req.text
+soup = BeautifulSoup(html, "html.parser")
+
+title_list = []
+link_list = []
+register_date_list = []
+modified_date_list = []
+new_register_index = []
+new_modified_index = []
+
+titles = soup.select('div.board-text > a')
+links = soup.select('div.board-text > a')
+register_dates = soup.select('p.info')
+modified_dates = soup.select('p.info')
+
+for title in titles:
+    title = title.text
+    title = ''.join(title.split())
+    title_list.append(title)
+for link in links:
+    link = link.get('href')
+    link_list.append(link)
+for register_date in register_dates:
+    register_date_list.append(register_date.text.split()[4])
+for modified_date in modified_dates:
+    modified_date_list.append(modified_date.text.split()[7])
+
+index = 0
+for check in register_date_list:
+    if check == str(datetime.date.today()):
+        new_register_index.append(index)
+    index += 1
+index = 0
+for check in modified_date_list:
+    if check == str(datetime.date.today()):
+        new_modified_index.append(index)
+    index += 1
+
+new_title = []
+print("신규등록")
+for i in new_register_index:
+    new_title.append(title_list[i])
+    print(title_list[i])
+print("신규수정")
+for i in new_modified_index:
+    if title_list[i] in new_title:
+        continue
+    print(title_list[i])
