2b5dc8bb9ed7e7e1ca95688722f104cd4138cfe6
kwjinwoo <38836328+kwjinwoo@users.noreply.github.com>
2020-07-03 22:31:30 +0900

    test용
    새롭게 추가된 제목을 얻는데 성공
    수정된 것을 얻는 것은 좀더 고민 해봐야할듯.(주석좀 달아줘 ㅠㅠ)

--- main.py
+++ main.py
@@ -9,20 +9,12 @@ soup = BeautifulSoup(html, "html.parser")
 
 title_list = []
 link_list = []
-register_date_list = []
-modified_date_list = []
-new_register_index = []
-new_modified_index = []
 
-#titles = soup.select('div.board-text > a')
-#links = soup.select('div.board-text > a')
-register_dates = soup.select('p.info')
-modified_dates = soup.select('p.info')
 divs = soup.select('div.board-text > a')
 
 for div in divs:
     title = ''.join(div.text.split())
-    title_list.append(title)
+    title_list.append(title.replace("신규게시글", '').replace("Attachment", ''))
     link_list.append(div.get('href'))
     
 tit = []
@@ -31,66 +23,33 @@ tit_sa = []
 tit_re = []
 tit_sa_modi = []
 
-for i in range(5):
-    i = 0
+for i in range(5):      #추출된 제목 리스트에서 5개만 tit리스트에 넣습니다.
     tit.append(title_list[i])
     tit_sa.append(title_list[i])
-    i += 1
-for modified_date in modified_dates:
-    modified_date_list.append(modified_date.text.split()[7])
-print(type(modified_date.text.split()[7]))
-s = 0                  
-for i in tit: 
-    if s == 3:
-        tit_sa[s] = '가가가가가가'
-    if tit_sa[s] not in tit:
-        new_title.append(tit_sa[s])
-    else:
-        j = 0
-        for i in title_list:
-            if i in tit_sa[s] and i in tit[s]:
-                tit_sa_modi.append(i.text.split()[7]) #수정된 날짜
-                tit_re.append(i.text.split()[4]) #등록된 날짜
-                if tit_sa_modi != tit_re:
-                    new_title.append(tit_sa)
-            j += 1        
-    s = s + 1
+
+tit_sa = tit[:]    #저장용 제목 리스트를 추출된 제목 리스트로 초기화 합니다. =로 단순 대입하면 주소값을 복제 하기에 tit이 바뀌면 tit_sa도 바뀜
+#s = 0
+#for i in tit:
+#if s == 3:
+tit[3] = '가가가가가가'   #의도적으로 tit의 3번째 요소를 변경합니다. / 3번 째 requests.get에서 title의 목록이 변경되었다는 것을 의미
+for check in tit:
+    if check not in tit_sa:     #tit와 tit_sa를 비교하여 tit의 새로운 요소를 new_title에 추가합니다.
+        new_title.append(check)
+        tit_sa = tit    #새로운 요소를 추가한 후 저장리스트를 다시 저장합니다.
+        '''
+        수정된 날짜를 확인하여 새로운 제목리스트에 추가하는 것 같은데 구현 방법이 잘 이해가 안되고 생각이 안나서 주석처리함
+        
+        else:
+            j = 0
+            for i in title_list:
+                if i in tit_sa[s] and i in tit[s]:
+                    tit_sa_modi.append(i.text.split()[7]) #수정된 날짜
+                    tit_re.append(i.text.split()[4]) #등록된 날짜
+                    if tit_sa_modi != tit_re:
+                        new_title.append(tit_sa)
+                j += 1
+         '''
+    #s += 1
     
 print(new_title)
 
-'''   
-for title in titles:
-    title = title.text
-    title = ''.join(title.split())
-    title_list.append(title)
-for link in links:
-    link = link.get('href')
-    link_list.append(link)
-  
-for register_date in register_dates:
-    register_date_list.append(register_date.text.split()[4])
-for modified_date in modified_dates:
-    modified_date_list.append(modified_date.text.split()[7])
-
-index = 0
-for check in register_date_list:
-    if check == str(datetime.date.today()):
-        new_register_index.append(index)
-    index += 1
-index = 0
-for check in modified_date_list:
-    if check == str(datetime.date.today()):
-        new_modified_index.append(index)
-    index += 1
-
-new_title = []
-print("신규등록")
-for i in new_register_index:
-    new_title.append(title_list[i])
-    #print(title_list[i])
-print("신규수정")
-for i in new_modified_index:
-    if title_list[i] in new_title:
-        continue
-    #print(title_list[i])
-'''
