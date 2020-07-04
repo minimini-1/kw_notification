6a095d9d6baf22aa1f01cae02f1f4923cdb9227a
kwjinwoo <38836328+kwjinwoo@users.noreply.github.com>
2020-07-04 21:00:57 +0900

    Update main.py
    인덱스를 통한 제목의 추가 및 수정된것을 추가하는 코드를 작성함

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import datetime
 url = 'https://www.kw.ac.kr/ko/life/notice.jsp'
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
 req = requests.get(url, headers=header)
@@ -14,8 +14,9 @@ register_date_list = []
 modified_date_list = []
 
 while True:
+    new_index = []
     new_title = []
-
+    new_link = []
     req = requests.get(url, headers=header)
     html = req.text
     soup = BeautifulSoup(html, "html.parser")
@@ -37,5 +38,12 @@ while True:
     if saved_title_list != title_list:  #기존에 저장된 데이터와 비교
         for check in title_list:
             if check not in saved_title_list:
-                new_title.append(check)
+                new_index.append(title_list.index(check))
+            else:
+                for i in range(len(register_date_list)):    #등록된 날짜 리스트 길이만큼 반복문 실행
+                    if register_date_list[i] != modified_date_list[i] and modified_date_list[i] == datetime.date.today(): # 수정된 날이 다르고 현재 날짜와 다름
+                        new_index.append(modified_date_list.index(modified_date_list[i]))
         saved_title_list = title_list[:]
+    for index in new_index:     #인덱스를 통한 새로운 제목과 링크를 추가
+        new_title.append(title_list[index])
+        new_link.append(link_list[index])
